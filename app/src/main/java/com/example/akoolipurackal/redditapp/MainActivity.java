20fd9dba8ed100d9c08831d585911ada84fde22b
Abey Koolipurackal <abey@virginia.edu>
2017-07-13 16:35:54 -0400

    Updated comments.

--- app/src/main/java/com/example/akoolipurackal/redditapp/MainActivity.java
+++ app/src/main/java/com/example/akoolipurackal/redditapp/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.akoolipurackal.redditapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.akoolipurackal.redditapp.model.Feed;
+
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final String BASE_URL = "https://www.reddit.com/r/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+
+        FeedAPI feedAPI = retrofit.create(FeedAPI.class);
+        retrofit2.Call<Feed> call = feedAPI.getFeed();
+        call.enqueue(new Callback<Feed>() {
+            @Override
+            public void onResponse(retrofit2.Call<Feed> call, Response<Feed> response) {
+                Log.d(TAG, "onResponse: feed:" + response.body().toString());
+                Log.d(TAG, "onResponse: Server Response:" + response.toString());
+
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<Feed> call, Throwable t) {
+                Log.e(TAG, "onFailure: Unable to retrive RSS:" + t.getMessage() );
+                Toast.makeText(MainActivity.this, "An Error Occured", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
